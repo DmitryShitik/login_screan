376b24e1e3c90a10ad9db264af4e0b25b58cb2ce
Dmitry Shitik <ximikos93@gmail.com>
2025-05-03 23:28:01 +0300

    create new activity, add intent

--- app/src/main/java/ru/dimitry/myapplication/MainActivity.java
+++ app/src/main/java/ru/dimitry/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package ru.dimitry.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText login;
+    EditText password;
+    Button btnLogin;
+
+    public void init(){
+        login = findViewById(R.id.input_login);
+        password = findViewById(R.id.input_pass);
+        btnLogin = findViewById(R.id.btn_login);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        btnLogin.setOnClickListener(view->{
+            Intent intent = new Intent(this, MainActivity2.class);
+            intent.putExtra("login",login.getText().toString());
+            intent.putExtra("password",btnLogin.getText().toString());
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/ru/dimitry/myapplication/MainActivity2.java
+++ app/src/main/java/ru/dimitry/myapplication/MainActivity2.java
@@ -3,12 +3,22 @@ package ru.dimitry.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView messageText;
+    Button btnBack;
+    public void init(){
+        messageText = findViewById(R.id.label_receive_date);
+        btnBack = findViewById(R.id.btn_back);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        init();
+
     }
 }
